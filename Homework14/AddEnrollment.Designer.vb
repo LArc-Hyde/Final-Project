bc38720f9bdf92f8a494975d8e5588fa5c69a8c0
Bryan Toscano <bt66099@student.sac.edu>
2022-12-03 16:51:37 -0800

    made add enrollment look nicer

--- Homework14/AddEnrollment.Designer.vb
+++ Homework14/AddEnrollment.Designer.vb
@@ -22,9 +22,160 @@ Partial Class AddEnrollment
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lstNotEnrolled = New System.Windows.Forms.ListBox()
+        Me.lstEnrolled = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.txtSeats = New System.Windows.Forms.TextBox()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 31)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(250, 23)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Courses:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 75)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(201, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Students Not Enrolled to this Course:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(474, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(178, 15)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Students Enrolled to this Course:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(474, 49)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 15)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Seats Availiable:"
+        '
+        'lstNotEnrolled
+        '
+        Me.lstNotEnrolled.FormattingEnabled = True
+        Me.lstNotEnrolled.ItemHeight = 15
+        Me.lstNotEnrolled.Location = New System.Drawing.Point(12, 93)
+        Me.lstNotEnrolled.Name = "lstNotEnrolled"
+        Me.lstNotEnrolled.Size = New System.Drawing.Size(250, 199)
+        Me.lstNotEnrolled.TabIndex = 6
+        '
+        'lstEnrolled
+        '
+        Me.lstEnrolled.FormattingEnabled = True
+        Me.lstEnrolled.ItemHeight = 15
+        Me.lstEnrolled.Location = New System.Drawing.Point(474, 93)
+        Me.lstEnrolled.Name = "lstEnrolled"
+        Me.lstEnrolled.Size = New System.Drawing.Size(250, 199)
+        Me.lstEnrolled.TabIndex = 7
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(299, 116)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(129, 50)
+        Me.btnAdd.TabIndex = 8
+        Me.btnAdd.Text = "Add to Course" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "--->"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Location = New System.Drawing.Point(608, 315)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(116, 33)
+        Me.btnBack.TabIndex = 9
+        Me.btnBack.Text = "Back"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(474, 315)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(116, 33)
+        Me.btnCreate.TabIndex = 10
+        Me.btnCreate.Text = "Create Enrollments"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'txtSeats
+        '
+        Me.txtSeats.Enabled = False
+        Me.txtSeats.Location = New System.Drawing.Point(582, 46)
+        Me.txtSeats.Name = "txtSeats"
+        Me.txtSeats.Size = New System.Drawing.Size(32, 23)
+        Me.txtSeats.TabIndex = 11
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(299, 209)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(129, 50)
+        Me.btnRemove.TabIndex = 12
+        Me.btnRemove.Text = "Remove From Course" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "<---"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'AddEnrollment
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(735, 368)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.txtSeats)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstEnrolled)
+        Me.Controls.Add(Me.lstNotEnrolled)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "AddEnrollment"
         Me.Text = "AddEnrollment"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents lstNotEnrolled As ListBox
+    Friend WithEvents lstEnrolled As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnBack As Button
+    Friend WithEvents btnCreate As Button
+    Friend WithEvents txtSeats As TextBox
+    Friend WithEvents btnRemove As Button
 End Class
--- Homework14/AddEnrollment.vb
+++ Homework14/AddEnrollment.vb
@@ -1,3 +1,73 @@
 ﻿Public Class AddEnrollment
+    Private Sub AddEnrollment_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If main.enroll.getStudentCount = 0 Or main.enroll.InstructorCount = 0 Or
+                main.enroll.CourseCount = 0 Then
+            MessageBox.Show("Students, Instructors, and Courses must all be created before moving to this page.")
+            Creator.Show()
+            Me.Close() ' forces closes if any list is empty
+        Else
+            For iter As Integer = 0 To main.enroll.CourseCount - 1 'populates combo box with Courses
+                ComboBox1.Items.Add(main.enroll.Courses(iter).CourseName & " " & main.enroll.Courses(iter).CourseNumber)
+            Next
+        End If
+    End Sub
 
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        'clears list boxes for next selection
+        lstNotEnrolled.Items.Clear()
+        lstEnrolled.Items.Clear()
+        txtSeats.Clear()
+
+        Dim courseNum() As String = Split(ComboBox1.SelectedText) 'course number will be stored in courseNum(1)
+        Dim hits As New List(Of Integer) 'will use this to save Student Numbers and populate list box of students not enrolled in course
+        Seats() 'changing txtSeats to selected
+        If main.enroll.EnrollmentCount <> 0 Then 'if it is not empty then will make search for students enrolled
+            For iter As Integer = 0 To main.enroll.EnrollmentCount - 1
+                If CInt(courseNum(1)) = main.enroll.GetEnroll(iter).Courses(0).CourseNumber Then 'adds Full name and Student Number to enrolled list box
+                    lstEnrolled.Items.Add(main.enroll.GetEnroll(iter).Students(0).FirstName & " " &
+                        main.enroll.GetEnroll(iter).Students(0).LastName & " " &
+                        main.enroll.GetEnroll(iter).Students(0).StudentNumber)
+                    hits.Add(main.enroll.GetEnroll(iter).Students(0).StudentNumber) 'matches addded to hits
+                End If
+            Next
+        End If
+
+        Dim isHit As Boolean 'if false will add Student to listbox of nonenrolled students; if true then will not get added
+
+        If hits.Count <> 0 Then
+            For iter1 As Integer = 0 To main.enroll.getStudentCount - 1
+                isHit = False 'must be reset for every student
+                For iter2 As Integer = 0 To hits.Count - 1
+                    If hits(iter2) = main.enroll.Students(iter1).StudentNumber Then
+                        isHit = True
+                    End If
+                Next
+                If isHit = False Then 'if no matched found will add to listbox
+                    lstNotEnrolled.Items.Add(main.enroll.Students(iter1).FirstName & " " &
+                                             main.enroll.Students(iter1).LastName & " " &
+                                             main.enroll.Students(iter1).StudentNumber)
+                End If
+            Next
+        End If
+    End Sub
+
+    Private Sub Seats()
+        Dim courseNum() As String = Split(ComboBox1.SelectedText)
+        Dim seatNum As Integer
+        For iter As Integer = 0 To main.enroll.CourseCount - 1
+            If (CInt(courseNum(1)) = main.enroll.Courses(iter).CourseNumber) Then
+                seatNum = main.enroll.Courses(iter).SeatsOpen
+            End If
+        Next
+        txtSeats.Text = CStr(seatNum)
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        Creator.Show()
+        Me.Close()
+    End Sub
 End Class
