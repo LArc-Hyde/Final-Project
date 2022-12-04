87bfa6f906250da73e7f70d9a1d097aed3e1b250
Bryan Toscano <bt66099@student.sac.edu>
2022-12-04 00:46:57 -0800

    removed testing code.

--- Homework14/AddEnrollment.vb
+++ Homework14/AddEnrollment.vb
@@ -12,53 +12,7 @@
         End If
     End Sub
 
-    'Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
-    '    'clears list boxes for next selection
-    '    lstNotEnrolled.Items.Clear()
-    '    lstEnrolled.Items.Clear()
-    '    txtSeats.Clear()
-
-    '    Dim courseNum() As String = Split(ComboBox1.SelectedItem.ToString) 'course number will be stored in courseNum(1)
-    '    Dim hits As New List(Of Integer) 'will use this to save Student Numbers and populate list box of students not enrolled in course
-    '    Seats() 'changing txtSeats to selected
-
-    '    If main.enroll.EnrollmentCount > 0 Then 'if it is not empty then will make search for students enrolled
-    '        For iter As Integer = 0 To main.enroll.EnrollmentCount - 1
-    '            If CInt(courseNum(1)) = main.enroll.GetEnroll(iter).Courses(0).CourseNumber Then 'adds Full name and Student Number to enrolled list box
-    '                lstEnrolled.Items.Add(main.enroll.GetEnroll(iter).Students(0).FirstName & " " &
-    '                    main.enroll.GetEnroll(iter).Students(0).LastName & " " &
-    '                    main.enroll.GetEnroll(iter).Students(0).StudentNumber)
-    '                hits.Add(main.enroll.GetEnroll(iter).Students(0).StudentNumber) 'matches addded to hits
-    '            End If
-    '        Next
-    '    Else
-    '        For iterEmpty As Integer = 0 To main.enroll.getStudentCount - 1
-    '            lstNotEnrolled.Items.Add(main.enroll.Students(iterEmpty).FirstName & " " &
-    '                    main.enroll.Students(iterEmpty).LastName & " " &
-    '                    main.enroll.Students(iterEmpty).StudentNumber)
-    '        Next
-    '    End If
-
-    '    Dim isHit As Boolean 'if false will add Student to listbox of nonenrolled students; if true then will not get added
-
-    '    If hits.Count > 0 Then
-    '        For iter1 As Integer = 0 To main.enroll.getStudentCount - 1
-    '            isHit = False 'must be reset for every student
-    '            For iter2 As Integer = 0 To hits.Count - 1
-    '                If hits(iter2) = main.enroll.Students(iter1).StudentNumber Then
-    '                    isHit = True
-    '                End If
-    '            Next
-    '            If isHit = False Then 'if no matched found will add to listbox
-    '                lstNotEnrolled.Items.Add(main.enroll.Students(iter1).FirstName & " " &
-    '                                         main.enroll.Students(iter1).LastName & " " &
-    '                                         main.enroll.Students(iter1).StudentNumber)
-    '            End If
-    '        Next
-    '    End If
-    'End Sub
-
-    'checks Course Number of currently selected Course and retrieves and sets the avaible seats
+    'checks Course Number Of currently selected Course And retrieves And sets the avaible seats
     Private Sub Seats()
         Dim courseNum() As String = Split(ComboBox1.SelectedItem.ToString)
         Dim seatNum As Integer
@@ -122,10 +76,11 @@
             MessageBox.Show("The Students Enrolled Box must have at least one student to create an enrollment.")
         End If
 
-        'lstNotEnrolled.Items.Clear()
-        'lstEnrolled.Items.Clear()
-        'txtSeats.Clear()
-        'ComboBox1.SelectedIndex = -1
+        'must search again
+        lstNotEnrolled.Items.Clear()
+        lstEnrolled.Items.Clear()
+        txtSeats.Clear()
+        ComboBox1.SelectedIndex = -1
     End Sub
 
     Private Sub btnSearch_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
@@ -148,16 +103,12 @@
                     hits.Add(main.enroll.GetEnroll(iter).Students(0).StudentNumber) 'matches addded to hits
                 End If
             Next
-        Else
-            For iterEmpty As Integer = 0 To main.enroll.getStudentCount - 1
-                'lstNotEnrolled.Items.Add(ComboBox1.SelectedItem.ToString)
-                lstNotEnrolled.Items.Add(main.enroll.Students(iterEmpty).FirstName & " " &
-                        main.enroll.Students(iterEmpty).LastName & " " &
-                        main.enroll.Students(iterEmpty).StudentNumber)
-            Next
         End If
 
         Dim isHit As Boolean 'if false will add Student to listbox of nonenrolled students; if true then will not get added
+        For i As Integer = 0 To hits.Count - 1
+            MessageBox.Show(hits(i))
+        Next
 
         If hits.Count > 0 Then
             For iter1 As Integer = 0 To main.enroll.getStudentCount - 1
@@ -172,6 +123,12 @@
                                              main.enroll.Students(iter1).LastName & " " &
                                              main.enroll.Students(iter1).StudentNumber)
                 End If
+            Next
+        Else
+            For iter2 As Integer = 0 To main.enroll.getStudentCount - 1
+                lstNotEnrolled.Items.Add(main.enroll.Students(iter2).FirstName & " " &
+                        main.enroll.Students(iter2).LastName & " " &
+                        main.enroll.Students(iter2).StudentNumber)
             Next
         End If
     End Sub
