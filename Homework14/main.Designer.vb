6ce57649e5648bf0e69e0e77ac1ab8d1b3327bb8
Bryan Toscano <bt66099@student.sac.edu>
2022-12-03 21:12:14 -0800

    finished main form

--- Homework14/main.Designer.vb
+++ Homework14/main.Designer.vb
@@ -22,92 +22,93 @@ Partial Class main
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.btnAddIStudent = New System.Windows.Forms.Button()
-        Me.btnAddInstructor = New System.Windows.Forms.Button()
-        Me.btnAddCourse = New System.Windows.Forms.Button()
-        Me.btnSave = New System.Windows.Forms.Button()
         Me.btnQuit = New System.Windows.Forms.Button()
         Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
-        Me.lstEnrollView = New System.Windows.Forms.ListBox()
+        Me.btnViewer = New System.Windows.Forms.Button()
+        Me.btnCreator = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
-        '
-        'btnAddIStudent
-        '
-        Me.btnAddIStudent.Location = New System.Drawing.Point(28, 35)
-        Me.btnAddIStudent.Name = "btnAddIStudent"
-        Me.btnAddIStudent.Size = New System.Drawing.Size(112, 43)
-        Me.btnAddIStudent.TabIndex = 0
-        Me.btnAddIStudent.Text = "Add Student"
-        Me.btnAddIStudent.UseVisualStyleBackColor = True
-        '
-        'btnAddInstructor
-        '
-        Me.btnAddInstructor.Location = New System.Drawing.Point(28, 175)
-        Me.btnAddInstructor.Name = "btnAddInstructor"
-        Me.btnAddInstructor.Size = New System.Drawing.Size(112, 43)
-        Me.btnAddInstructor.TabIndex = 1
-        Me.btnAddInstructor.Text = "Add Instructor"
-        Me.btnAddInstructor.UseVisualStyleBackColor = True
-        '
-        'btnAddCourse
-        '
-        Me.btnAddCourse.Location = New System.Drawing.Point(28, 106)
-        Me.btnAddCourse.Name = "btnAddCourse"
-        Me.btnAddCourse.Size = New System.Drawing.Size(112, 43)
-        Me.btnAddCourse.TabIndex = 2
-        Me.btnAddCourse.Text = "Add Course"
-        Me.btnAddCourse.UseVisualStyleBackColor = True
-        '
-        'btnSave
-        '
-        Me.btnSave.Location = New System.Drawing.Point(28, 245)
-        Me.btnSave.Name = "btnSave"
-        Me.btnSave.Size = New System.Drawing.Size(112, 43)
-        Me.btnSave.TabIndex = 3
-        Me.btnSave.Text = "Save"
-        Me.btnSave.UseVisualStyleBackColor = True
         '
         'btnQuit
         '
-        Me.btnQuit.Location = New System.Drawing.Point(28, 309)
+        Me.btnQuit.Location = New System.Drawing.Point(26, 264)
         Me.btnQuit.Name = "btnQuit"
         Me.btnQuit.Size = New System.Drawing.Size(112, 43)
         Me.btnQuit.TabIndex = 4
         Me.btnQuit.Text = "Quit"
         Me.btnQuit.UseVisualStyleBackColor = True
         '
-        'lstEnrollView
+        'btnViewer
         '
-        Me.lstEnrollView.FormattingEnabled = True
-        Me.lstEnrollView.ItemHeight = 15
-        Me.lstEnrollView.Location = New System.Drawing.Point(219, 35)
-        Me.lstEnrollView.Name = "lstEnrollView"
-        Me.lstEnrollView.Size = New System.Drawing.Size(301, 319)
-        Me.lstEnrollView.TabIndex = 5
+        Me.btnViewer.Location = New System.Drawing.Point(26, 31)
+        Me.btnViewer.Name = "btnViewer"
+        Me.btnViewer.Size = New System.Drawing.Size(112, 43)
+        Me.btnViewer.TabIndex = 5
+        Me.btnViewer.Text = "View Enrollments"
+        Me.btnViewer.UseVisualStyleBackColor = True
+        '
+        'btnCreator
+        '
+        Me.btnCreator.Location = New System.Drawing.Point(26, 149)
+        Me.btnCreator.Name = "btnCreator"
+        Me.btnCreator.Size = New System.Drawing.Size(112, 43)
+        Me.btnCreator.TabIndex = 6
+        Me.btnCreator.Text = "Enter Creator"
+        Me.btnCreator.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(199, 40)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(185, 15)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Click to View Created Enrollments"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(199, 149)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(227, 45)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Click to Create Students, Instructors," & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Courses, and Enrollments.  UIser can Load" &
+    "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "and Save as well."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(199, 278)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(121, 15)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Click to exit program."
         '
         'main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(557, 364)
-        Me.Controls.Add(Me.lstEnrollView)
+        Me.ClientSize = New System.Drawing.Size(446, 364)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCreator)
+        Me.Controls.Add(Me.btnViewer)
         Me.Controls.Add(Me.btnQuit)
-        Me.Controls.Add(Me.btnSave)
-        Me.Controls.Add(Me.btnAddCourse)
-        Me.Controls.Add(Me.btnAddInstructor)
-        Me.Controls.Add(Me.btnAddIStudent)
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "main"
-        Me.Text = "Add Enrollment"
+        Me.Text = "Start Page"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents btnAddIStudent As Button
-    Friend WithEvents btnAddInstructor As Button
-    Friend WithEvents btnAddCourse As Button
-    Friend WithEvents btnSave As Button
     Friend WithEvents btnQuit As Button
     Friend WithEvents SaveFileDialog1 As SaveFileDialog
-    Friend WithEvents lstEnrollView As ListBox
+    Friend WithEvents btnViewer As Button
+    Friend WithEvents btnCreator As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
 End Class
--- Homework14/main.vb
+++ Homework14/main.vb
@@ -2,43 +2,17 @@
 
 Public Class main
     Public enroll As New enrollments
-    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
-        'saving should only be available when a 1 COURSE, 1 STUDENT, & 1 INSTRUCTOR ARE LOADED
-        SaveFileDialog1.ShowDialog()
-        Dim location As String = SaveFileDialog1.FileName
-        enroll.SaveData(location)
-
-        'getting main for ready for another enrollment
-        btnSave.Enabled = False
-        btnAddIStudent.Enabled = True 'can now add student
-        lstEnrollView.Items.RemoveAt(lstEnrollView.Items.Count - 1) 'removing last instruction
-        lstEnrollView.Items.Add("Enrollment saved-----------------------------------------------------------------------------")
-    End Sub
-
     Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
         Me.Close()
     End Sub
 
-    Private Sub main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        'Will start disabled but will be enabled when user goes through desired path
-        btnAddInstructor.Enabled = False
-        btnAddCourse.Enabled = False
-        btnSave.Enabled = False
-        lstEnrollView.Items.Add("Must Add Student Next")
+    Private Sub btnCreator_Click(sender As Object, e As EventArgs) Handles btnCreator.Click
+        Me.Hide()
+        Creator.Show()
     End Sub
 
-    Private Sub btnAddIStudent_Click(sender As Object, e As EventArgs) Handles btnAddIStudent.Click
+    Private Sub btnViewer_Click(sender As Object, e As EventArgs) Handles btnViewer.Click
         Me.Hide()
-        addStudent.Show() 'opens addStudent Form
-    End Sub
-
-    Private Sub btnAddCourse_Click(sender As Object, e As EventArgs) Handles btnAddCourse.Click
-        Me.Hide()
-        addCourse.Show() 'opens addCourse form
-    End Sub
-
-    Private Sub btnAddInstructor_Click(sender As Object, e As EventArgs) Handles btnAddInstructor.Click
-        Me.Hide()
-        addInstructor.show() 'opens addInstructor form
+        Viewer.Show()
     End Sub
 End Class
