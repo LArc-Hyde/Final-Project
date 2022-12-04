faad83b19f3c89f0f44f79c6207a2a8311e4a62d
Bryan Toscano <bt66099@student.sac.edu>
2022-12-03 21:38:58 -0800

    Finished Viewer Form

--- Homework14/Viewer.Designer.vb
+++ Homework14/Viewer.Designer.vb
@@ -22,9 +22,91 @@ Partial Class Viewer
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.btnStudents = New System.Windows.Forms.Button()
+        Me.btnInstructors = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnStudents
+        '
+        Me.btnStudents.Location = New System.Drawing.Point(36, 39)
+        Me.btnStudents.Name = "btnStudents"
+        Me.btnStudents.Size = New System.Drawing.Size(124, 52)
+        Me.btnStudents.TabIndex = 0
+        Me.btnStudents.Text = "Show Students"
+        Me.btnStudents.UseVisualStyleBackColor = True
+        '
+        'btnInstructors
+        '
+        Me.btnInstructors.Location = New System.Drawing.Point(36, 122)
+        Me.btnInstructors.Name = "btnInstructors"
+        Me.btnInstructors.Size = New System.Drawing.Size(124, 52)
+        Me.btnInstructors.TabIndex = 1
+        Me.btnInstructors.Text = "Show Instructors"
+        Me.btnInstructors.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(36, 207)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(124, 52)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Show Courses"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(36, 289)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(124, 52)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Show Enrollments"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(36, 360)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(124, 52)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Back"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(215, 39)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(538, 373)
+        Me.DataGridView1.TabIndex = 5
+        '
+        'Viewer
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Viewer"
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.btnInstructors)
+        Me.Controls.Add(Me.btnStudents)
+        Me.Name = "Viewer"
+        Me.Text = "View Enrollments"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
+
+    Friend WithEvents btnStudents As Button
+    Friend WithEvents btnInstructors As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- Homework14/Viewer.vb
+++ Homework14/Viewer.vb
@@ -1,3 +1,31 @@
 ﻿Public Class Viewer
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        main.Show()
+        Me.Close()
+    End Sub
 
+    Private Sub btnStudents_Click(sender As Object, e As EventArgs) Handles btnStudents.Click
+        DataGridView1.DataSource = main.enroll.Students
+    End Sub
+
+    Private Sub btnInstructors_Click(sender As Object, e As EventArgs) Handles btnInstructors.Click
+        DataGridView1.DataSource = main.enroll.Instructors
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        DataGridView1.DataSource = main.enroll.Courses
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        DataGridView1.DataSource = main.enroll.AllEnrollments
+    End Sub
+
+    Private Sub Viewer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If main.enroll.EnrollmentCount = 0 Or main.enroll.getStudentCount = 0 Or
+                main.enroll.InstructorCount = 0 Or main.enroll.CourseCount = 0 Then
+            MessageBox.Show("Student List, Instructor List, Course List, and Enrollment List must not be empty")
+            main.Show()
+            Me.Close()
+        End If
+    End Sub
 End Class
