42121b62e4096f8cfea8e535997f29134a14649e
Bryan Toscano <kevin1bryan2@gmail.com>
2022-12-06 15:36:30 -0800

    commented out Testing Code

--- Homework14/main.vb
+++ Homework14/main.vb
@@ -28,44 +28,45 @@ Public Class main
     End Sub
 
     Private Sub main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        st1.FirstName() = "Bryan"
-        st1.LastName() = "Toscano"
-        st1.Email() = "bt@mail.com"
-        st1.StudentNumber = 313
+        'THIS IS TESTING CODE
+        'st1.FirstName() = "Bryan"
+        'st1.LastName() = "Toscano"
+        'st1.Email() = "bt@mail.com"
+        'st1.StudentNumber = 313
 
-        st2.FirstName() = "Nathen"
-        st2.LastName() = "Toscano"
-        st2.Email() = "nt@mail.com"
-        st2.StudentNumber = 461
+        'st2.FirstName() = "Nathen"
+        'st2.LastName() = "Toscano"
+        'st2.Email() = "nt@mail.com"
+        'st2.StudentNumber = 461
 
-        in1.FirstName() = "Joe"
-        in1.LastName() = "Biden"
-        in1.Email() = "jb@mail.com"
-        in1.OfficeNumber = 115
+        'in1.FirstName() = "Joe"
+        'in1.LastName() = "Biden"
+        'in1.Email() = "jb@mail.com"
+        'in1.OfficeNumber = 115
 
-        in2.FirstName() = "George"
-        in2.LastName() = "Washington"
-        in2.Email() = "gw@mail.com"
-        in2.OfficeNumber = 116
+        'in2.FirstName() = "George"
+        'in2.LastName() = "Washington"
+        'in2.Email() = "gw@mail.com"
+        'in2.OfficeNumber = 116
 
-        co1.CourseNumber() = 999
-        co1.RoomNumber() = 215
-        co1.CourseName() = "Comp105"
-        co1.Units() = 3
-        co1.Professor("Joe", "Biden")
+        'co1.CourseNumber() = 999
+        'co1.RoomNumber() = 215
+        'co1.CourseName() = "Comp105"
+        'co1.Units() = 3
+        'co1.Professor("Joe", "Biden")
 
-        co2.CourseNumber() = 888
-        co2.RoomNumber() = 216
-        co2.CourseName() = "Eng101"
-        co2.Units() = 3
-        co2.Professor("George", "Washington")
+        'co2.CourseNumber() = 888
+        'co2.RoomNumber() = 216
+        'co2.CourseName() = "Eng101"
+        'co2.Units() = 3
+        'co2.Professor("George", "Washington")
 
-        enroll.Students(st1)
-        enroll.Students(st2)
-        enroll.Instructors(in1)
-        enroll.Instructors(in2)
-        enroll.Courses(co1)
-        enroll.Courses(co2)
+        'enroll.Students(st1)
+        'enroll.Students(st2)
+        'enroll.Instructors(in1)
+        'enroll.Instructors(in2)
+        'enroll.Courses(co1)
+        'enroll.Courses(co2)
     End Sub
 
 
